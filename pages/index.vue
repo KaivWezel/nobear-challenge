ecc40297350b6f8da001f303d08e73da83a86095
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2023-12-21 15:12:25 +0100

    Main page for visualization

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,27 @@
 <template>
 	<div>
-		<h1>Index</h1>
-		<button @click="pagenum++">plus</button>
-		<button @click="pagenum--">minus</button>
-		<span>{{ pagenum }}</span>
-		<VacancyCard v-for="(hit, index) in data.hits.hits" :hit="hit" :index="index" />
+		<h1>Jobs</h1>
+		<div class="jobBoard">
+			<div class="filters">
+				<CategoryFilter
+					v-for="filter in filters"
+					:category="filter.title"
+					:values="filter.values"
+					@update:category="updateQuery" />
+			</div>
+			<div class="list">
+				<VacancyCard v-for="(hit, index) in data.hits.hits" :hit="hit" :index="index" />
+			</div>
+		</div>
 	</div>
 </template>
 <script setup>
+import filters from "~/assets/data/filters.json";
+
+const router = useRouter();
+
 const pagenum = ref(1);
+
 const { data } = await useFetch(`https://data.pro-cluster.com/jobs`, {
 	query: {
 		perpage: 13,
@@ -17,8 +30,16 @@ const { data } = await useFetch(`https://data.pro-cluster.com/jobs`, {
 	},
 });
 
+const query = ref({});
+
+function updateQuery({ category, values }) {
+	query.value[category] = values;
+	router.push({ query: query.value });
+}
+
 watch(pagenum, () => {
 	console.log(data.value.hits.hits[0]);
+	console.log(useQuery());
 });
 </script>
 <style lang="scss">
@@ -28,5 +49,10 @@ h1 {
 	span {
 		color: red;
 	}
+}
+
+.jobBoard {
+	display: grid;
+	grid-template-columns: 1fr 3fr;
 }
 </style>
