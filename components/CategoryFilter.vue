34e48ae06c9243bf733b3494864a3f07f9673b0d
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2023-12-21 15:12:16 +0100

    Basic filter logic in query

---
+++ components/CategoryFilter.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="filterCategory">
+		<h3>Category</h3>
+		<span>{{ checkedValues }}</span>
+		<div class="filterCategory__values">
+			<div class="filterCategory__value" v-for="(value, index) in props.values" :key="index">
+				<input type="checkbox" :id="value" :value="value" v-model="checkedValues" />
+				<label :for="value">{{ value }}</label>
+			</div>
+		</div>
+	</div>
+</template>
+<script setup>
+const route = useRoute();
+const props = defineProps({
+	category: {
+		type: String,
+		default: "category x",
+	},
+	values: {
+		type: Array,
+		default: [],
+	},
+});
+
+const checkedValues = ref([]);
+
+onMounted(() => {
+	setFilter();
+});
+
+const setFilter = () => {
+	if (!route.query[props.category]) {
+		checkedValues.value = [];
+	} else {
+		checkedValues.value = checkedValues.value.concat(route.query[props.category]);
+	}
+};
+
+const updateFilter = () => {
+	emit("update:category", { category: props.category, values: checkedValues.value });
+};
+
+watch(checkedValues, updateFilter);
+
+const emit = defineEmits(["update:category"]);
+</script>
+<style lang="scss"></style>
