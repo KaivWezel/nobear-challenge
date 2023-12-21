5972f68111c17cbd9ffdf177da4a7517400400d0
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2023-12-21 10:53:00 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="App">
+		<NuxtPage></NuxtPage>
+	</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h1>Kai van Wezel</h1>
+		<h2>Creative front end developer</h2>
+	</div>
+</template>
+<script setup>
+onMounted(() => {
+	console.log("mounted");
+});
+</script>
+<style lang="scss">
+h1 {
+	font-size: 3rem;
+
+	span {
+		color: red;
+	}
+}
+</style>
