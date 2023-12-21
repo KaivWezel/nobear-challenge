e4bc8cff3b82683d3c45045dc9ade5cf855843d9
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2023-12-21 11:55:40 +0100

    First fetch

---
+++ components/VacancyCard.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="vacancyCard">
+		<h2>Vacancy card {{ props.index + 1 }}</h2>
+		<h3>{{ hit._source.website.title }}</h3>
+	</div>
+</template>
+<script setup>
+const props = defineProps({
+	index: { type: Number, default: -1 },
+	hit: { default: {} },
+});
+</script>
+<style lang="scss"></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,24 @@
 <template>
 	<div>
-		<h1>Kai van Wezel</h1>
-		<h2>Creative front end developer</h2>
+		<h1>Index</h1>
+		<button @click="pagenum++">plus</button>
+		<button @click="pagenum--">minus</button>
+		<span>{{ pagenum }}</span>
+		<VacancyCard v-for="(hit, index) in data.hits.hits" :hit="hit" :index="index" />
 	</div>
 </template>
 <script setup>
-onMounted(() => {
-	console.log("mounted");
+const pagenum = ref(1);
+const { data } = await useFetch(`https://data.pro-cluster.com/jobs`, {
+	query: {
+		perpage: 13,
+		pagenum,
+		aggs: true,
+	},
+});
+
+watch(pagenum, () => {
+	console.log(data.value.hits.hits[0]);
 });
 </script>
 <style lang="scss">
