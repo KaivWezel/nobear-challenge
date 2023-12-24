7b0849a091879a77d96d8d7b280c7e1aec49a5aa
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2023-12-24 12:37:28 +0100

    Basic layout job card

--- components/VacancyCard.vue
+++ components/VacancyCard.vue
@@ -1,7 +1,15 @@
 <template>
-	<div class="vacancyCard">
-		<h2>Vacancy card {{ props.index + 1 }}</h2>
-		<h3>{{ hit._source.website.title }}</h3>
+	<div class="vacancyCard" :data-index="index">
+		<h2 class="vacancyCard__title">{{ website.title }}</h2>
+		<span class="vacancyCard__subtitle">{{ website.subtitle }}</span>
+
+		<div class="vacancyCard__description">{{ website.intro }}</div>
+		<div class="vacancyCard__grid">
+			<div class="location">{{ website.location.place }}</div>
+			<div class="salary">{{ vacancy.salary_indication.min }} tot {{ vacancy.salary_indication.max }}</div>
+			<div class="hours">{{ vacancy.shiftwork[0] }}</div>
+			<div class="sector">{{ website.sector?.[0] }}</div>
+		</div>
 	</div>
 </template>
 <script setup>
@@ -9,5 +17,38 @@ const props = defineProps({
 	index: { type: Number, default: -1 },
 	hit: { default: {} },
 });
+
+const website = computed(() => props.hit._source.website);
+const vacancy = computed(() => props.hit._source.vacancy);
 </script>
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.vacancyCard {
+	border: 1px solid black;
+	padding: 2.5rem 2rem;
+
+	&__title {
+		margin-bottom: 0.5rem;
+	}
+
+	&__subtitle {
+		display: block;
+		font-size: 1.2rem;
+		margin-bottom: 1rem;
+	}
+
+	&__description {
+		line-height: 1.2rem;
+		max-height: 3.6rem;
+		word-wrap: break-word;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		margin-bottom: 1.5rem;
+	}
+
+	&__grid {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 1rem;
+	}
+}
+</style>
