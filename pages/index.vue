8e708b79f6ee653125a6857ad9855046638709c0
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2023-12-26 11:24:34 +0100

    Quick way to get all different categories

--- pages/index.vue
+++ pages/index.vue
@@ -17,18 +17,43 @@
 </template>
 <script setup>
 import filters from "~/assets/data/filters.json";
-
+import useFiltered from "~/composables/useFiltered";
 const router = useRouter();
 const route = useRoute();
 
 const activeQuery = ref([]);
 
-const { data } = useFetch("/api/jobs", { query: activeQuery.value });
+const { data } = await useFetch("/api/jobs", { query: activeQuery.value });
+
+async function allCategories() {
+	let temp = [];
+	data.value.jobs.hits.hits.forEach((job) => {
+		const filter = job._source.website.category;
+		const values = Object.values(filter);
+		if (values.some((r) => temp.includes(r))) {
+			console.log("exists");
+		} else {
+			temp.push(...values);
+		}
+	});
+
+	console.log(temp);
+}
+
+onMounted(() => {
+	allCategories();
+});
+
+const filtered = useFiltered();
 
 function updateQuery({ category, values }) {
 	activeQuery.value[category] = values;
 	router.push({ query: activeQuery.value });
 }
+
+watch(filtered, () => {
+	console.log(filtered);
+});
 </script>
 <style lang="scss">
 h1 {
