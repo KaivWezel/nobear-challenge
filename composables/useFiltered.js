b18d28134e0af709f35642531bf134cd2b12ba99
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2023-12-27 10:58:35 +0100

    Add salary filter

--- composables/useFiltered.js
+++ composables/useFiltered.js
@@ -6,7 +6,7 @@ export default function useFiltered() {
 		const filterFunction = query?.functie || [];
 		const filterSector = query?.sector || [];
 		const filterShift = query?.ploegendienst || [];
-		const filterSalary = query?.salaris | [];
+		const filterSalary = Array.isArray(query.salaris) ? query?.salaris : [query?.salaris] || [];
 
 		const { data } = await useFetch("/api/jobs");
 
@@ -40,6 +40,28 @@ export default function useFiltered() {
 			const jobShift = job._source.vacancy.shiftwork;
 			if (filterShift.length && jobShift) {
 				return jobShift.some((f) => filterShift.includes(f));
+			} else {
+				return true;
+			}
+		});
+
+		// Filter for salary
+		temp = temp.filter((job) => {
+			const jobSalary = job._source.vacancy.salary_indication;
+			const min_sal = jobSalary.min;
+			const max_sal = jobSalary.max;
+			const filters = filterSalary.map((val) => {
+				const [min, max] = val.split("-");
+				return { min, max };
+			});
+			if (filterSalary.length) {
+				return filters.some((val) => {
+					if (val.min && val.max) {
+						return min_sal >= val.min && min_sal <= val.max;
+					} else {
+						return false;
+					}
+				});
 			} else {
 				return true;
 			}
