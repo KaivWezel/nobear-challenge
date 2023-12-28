d4be4a40c221ceab34bec02fc20f0717a058491d
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2023-12-28 10:44:32 +0100

    VacancyCard styling update

--- components/VacancyCard.vue
+++ components/VacancyCard.vue
@@ -1,17 +1,17 @@
 <template>
 	<nuxt-link :to="'/jobs/' + hit._id" class="vacancyCard" :data-index="index">
-		<h2 class="vacancyCard__title">{{ website.title }}</h2>
+		<h3 class="vacancyCard__title">{{ website.title }}</h3>
 		<span class="vacancyCard__subtitle">{{ website.subtitle }}</span>
 
 		<!-- <div class="vacancyCard__description">{{ website.intro }}</div> -->
 		<div class="vacancyCard__grid">
 			<div class="location">{{ website.location.place }}</div>
-			<div class="salary">{{ vacancy.salary_indication.min }} tot {{ vacancy.salary_indication.max }}</div>
+			<div class="salary">€{{ vacancy.salary_indication.min }} tot €{{ vacancy.salary_indication.max }}</div>
 			<div class="hours">{{ vacancy.shiftwork[0] }}</div>
 			<div class="sector">{{ website.category?.[0] }}</div>
 		</div>
 		<div class="vacancyCard__links">
-			<nuxt-link :to="'/jobs/' + hit._id">Bekijk vacature</nuxt-link>
+			<nuxt-link class="detail" :to="'/jobs/' + hit._id">Bekijk vacature</nuxt-link>
 		</div>
 	</nuxt-link>
 </template>
@@ -27,7 +27,8 @@ const vacancy = computed(() => props.hit._source.vacancy);
 <style lang="scss" scoped>
 @import "~/assets/styles/main.scss";
 .vacancyCard {
-	display: block;
+	display: grid;
+	flex-direction: column;
 	border: 1px solid rgba(black, 0.1);
 	width: 100%;
 	padding: 1.5rem 1rem;
@@ -38,19 +39,33 @@ const vacancy = computed(() => props.hit._source.vacancy);
 	color: $color-text;
 	background-color: $color-bg;
 
-	box-shadow: 2px 2px 10px 2px rgba(white, 0.1);
+	box-shadow: 3px 3px 10px 4px rgba($color-text, 0.1);
 
-	&:visited {
+	transition: all 0.3s ease-out;
+
+	&:hover {
+		border: 1px solid rgba(red, 0.5);
+		box-shadow: 2px 2px 8px 2px rgba($color-text, 0.05);
+
+		a::before {
+			transform: translate(1.2rem, -50%) rotate(45deg);
+		}
 	}
 
 	&__title {
 		margin-bottom: 1.5rem;
-		font-size: 1.2rem;
+		font-size: 1.5rem;
+		line-height: 1.25;
+
+		@include desktop {
+			font-size: 2rem;
+		}
 	}
 
 	&__subtitle {
 		display: block;
 		font-size: 1rem;
+		font-weight: 400;
 		margin-bottom: 1.5rem;
 		font-size: max(0.8rem, 10x);
 	}
@@ -69,6 +84,37 @@ const vacancy = computed(() => props.hit._source.vacancy);
 		grid-template-columns: repeat(2, 1fr);
 		gap: 0.5rem;
 		margin-bottom: 1.5rem;
+		font-weight: 200;
+		letter-spacing: 0.012rem;
+	}
+
+	&__links {
+		align-self: flex-end;
+
+		a {
+			text-decoration: none;
+			color: red;
+			position: relative;
+
+			&::before,
+			&::after {
+				display: block;
+				content: "";
+				position: absolute;
+			}
+
+			&::before {
+				width: 8px;
+				height: 8px;
+				border-style: solid;
+				border-width: 0.2rem 0.2rem 0 0;
+				right: 0;
+				top: 50%;
+				transform: translate(1rem, -50%) rotate(45deg);
+
+				transition: transform 0.2s cubic-bezier(0.5, 1, 0.89, 1);
+			}
+		}
 	}
 }
 </style>
